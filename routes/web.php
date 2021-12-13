e79bceb2e4f1513894d7a9caf21455c0f2d9b848
carson5131 <carson_198@hotmail.com>
2021-12-13 23:35:31 +0800

    test: test using classifierId to auto detect the template from baidu AI

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\OCRController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,5 +15,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('upload');
 });
+
+Route::get('/testAPI', [OCRController::class, 'uploadImage']);
+Route::post('/upload-image', [OCRController::class, 'uploadFile'])->name('upload-file');
